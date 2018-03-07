9ef24353b885e68d724b01ee03f512a0a14a2aa6
René Schimmelpfennig <mail@reneschimmelpfennig.de>
2018-03-07 21:18:01 +0100

    add function to format seconds to display clock format

--- Helper.h
+++ Helper.h
@@ -9,10 +9,26 @@
 
 #include <RtcDS1307.h>
 
+// macros from DateTime.h 
+/* Useful Constants */
+#define SECS_PER_MIN  (60UL)
+#define SECS_PER_HOUR (3600UL)
+#define SECS_PER_DAY  (SECS_PER_HOUR * 24L)
+
+/* Useful Macros for getting elapsed time */
+#define numberOfSeconds(_time_) (_time_ % SECS_PER_MIN)  
+#define numberOfMinutes(_time_) ((_time_ / SECS_PER_MIN) % SECS_PER_MIN) 
+#define numberOfHours(_time_) (( _time_% SECS_PER_DAY) / SECS_PER_HOUR)
+#define elapsedDays(_time_) ( _time_ / SECS_PER_DAY)  
+
 class Helper {
   public:
     static String getFormatedDateTime(const RtcDateTime& dt);
     static const char* floatToCharArray(float value);
     static String byteArrayToString(byte* bytes, unsigned int length);
+    static String getFormatedSeconds(uint32_t valueInSeconds);
+
+  private:
+    static String printDigits(byte digits);
 };
 #endif
